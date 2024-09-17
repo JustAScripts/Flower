c64d1d28c129162c7d7de32169b7adf29252250b
GrayNoCat <149206706+JustAScripts@users.noreply.github.com>
2024-09-17 08:27:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -32,9 +32,6 @@ tag = """⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣴⡦⠀�
 for line in tag.splitlines():
     print(Fore.MAGENTA + Style.BRIGHT + line.center(os.get_terminal_size().columns))
 
-from datetime import datetime
-from colorama import Fore, Style
-
 def warnings(message):
     print(Fore.CYAN + Style.BRIGHT + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + Style.RESET_ALL + Fore.YELLOW + Style.BRIGHT + ' [WARNING]' + Style.RESET_ALL + Style.BRIGHT + ' ' + message)
 
@@ -43,15 +40,39 @@ def error(error, message):
 
 def success(success, message):
     print(Fore.CYAN + Style.BRIGHT + datetime.now().strftime("%Y-%m-%d %H:%M:%S") + Style.RESET_ALL + Fore.GREEN + Style.BRIGHT + ' [' + success + ']' + Style.RESET_ALL + Style.BRIGHT + ' ' + message)
-    
+
+def thumbnail(asset):
+    request = requests.get(f"https://thumbnails.roblox.com/v1/assets?assetIds={asset}&returnPolicy=PlaceHolder&size=75x75&format=Png&isCircular=false")
+    return request.json()['data'][0]['imageUrl']
+
+def hookweb(c, asset):
+    return {
+        "content": f"<@{config['Discord']['ping']}>",
+        "embeds": [
+            {
+                "title": f"Successfully Bought {c}",
+                "url": f"https://www.roblox.com/catalog/{asset}",
+                "description": "",
+                "thumbnail": {
+                    "url": thumbnail(asset)
+                },
+                "footer": {
+                    "text": "Flower Limited Sniper V1",
+                    "icon_url": "https://cdn.discordapp.com/attachments/1175613436483350609/1285372373465567252/12640030.jpg?ex=66ea07ce&is=66e8b64e&hm=fc2520fcfea4a3a8c399d1e6ae81fe43f769a9a24cab2746f2f750a87dc10ccd&"
+                },
+                "color": 16761035
+            }
+        ]
+    }
+
 def validating():
     r = requests.get("https://users.roblox.com/v1/users/authenticated", headers={"Cookie": f".ROBLOSECURITY={config['Roblox']['Cookies']}"})
     if r.ok:
-        success('WORK','Roblox Cookies Working')
+        success('WORK', 'Roblox Cookies Working')
     else:
-        error('Failed','Your roblox token aren\'t valid please get it again.')
+        error('Failed', 'Your Roblox token isn\'t valid, please get it again.')
         sys.exit()       
-        
+
 validating()
 
 def xcrsf():
@@ -81,54 +102,60 @@ else:
 
 bar = None
 while True:
-        baz = requests.get("https://pastefy.app/Pq7EfNmH/raw")
-        qux = baz.json()
-        quux = qux['Paid']['id'] if value else qux['Web']['id']
+    baz = requests.get("https://pastefy.app/Pq7EfNmH/raw")
+    qux = baz.json()
+    quux = qux['Paid']['id'] if value else qux['Web']['id']
 
-        if bar is not None and quux != bar:
-            asset = quux
-            economy = requests.get(f'https://economy.roblox.com/v2/assets/{asset}/details')
-            if economy.ok:
-                info = economy.json()
-                payload = {
-                    "collectibleItemId": str(info['CollectibleItemId']),
-                    "collectibleProductId": str(info['CollectibleProductId']),
-                    "expectedCurrency": 1,
-                    "expectedPrice": str(info['PriceInRobux']),
-                    "idempotencyKey": str(uuid.uuid4()),
-                    "expectedSellerId": str(info['Creator']['Id']),
-                    "expectedSellerType": str(info['Creator']['CreatorType']),
-                    "expectedPurchaserType": "User",
-                    "expectedPurchaserId": user_id
-                }
+    if bar is not None and quux != bar:
+        asset = quux
+        economy = requests.get(f'https://economy.roblox.com/v2/assets/{asset}/details')
+        if economy.ok:
+            info = economy.json()
+            payload = {
+                "collectibleItemId": str(info['CollectibleItemId']),
+                "collectibleProductId": str(info['CollectibleProductId']),
+                "expectedCurrency": 1,
+                "expectedPrice": str(info['PriceInRobux']),
+                "idempotencyKey": str(uuid.uuid4()),
+                "expectedSellerId": str(info['Creator']['Id']),
+                "expectedSellerType": str(info['Creator']['CreatorType']),
+                "expectedPurchaserType": "User",
+                "expectedPurchaserId": user_id
+            }
 
-                def Post():
-                    global xcrsf_token
-                    Purchase = requests.post(
-                        f'https://apis.roblox.com/marketplace-sales/v1/item/{info["CollectibleItemId"]}/purchase-item',
-                        headers={'x-csrf-token': xcrsf_token},
-                        cookies={'.ROBLOSECURITY': security},
-                        json=payload
-                    )
-
+            def Post():
+                global xcrsf_token
+                Purchase = requests.post(
+                    f'https://apis.roblox.com/marketplace-sales/v1/item/{info["CollectibleItemId"]}/purchase-item',
+                    headers={'x-csrf-token': xcrsf_token},
+                    cookies={'.ROBLOSECURITY': security},
+                    json=payload
+                )
+ 
+                try:
                     respond = Purchase.json()
-                    
-                    if respond['purchased']:
-                        success('SUCCESS', f'Successfully bought {info["Name"]}')
-                    else:
-                        error('FAILED', respond['errorMessage'])
+                except requests.exceptions.JSONDecodeError:
+                    error('ERROR', 'Something went wrong with respond. Continue to Watch other will be fixed soon once root of error found.')
+                    return
 
-                if value:
-                    if info['PriceInRobux'] in config['Setting']['Prices']:
-                        Post()
-                    else:
-                        warnings("Price Doesn't Match")
+                if respond['purchased']:
+                    success('SUCCESS', f'Successfully bought {info["Name"]}')
+                    if config['Discord']['webhook']:
+                        ponse = requests.post(config['Discord']['url'], json=hookweb(info['Name'], asset))
                 else:
-                    if info['PriceInRobux'] == 0:
-                        for _ in range(5):
-                            Post()
+                    error('FAILED', respond['errorMessage'])
+
+            if value:
+                if info['PriceInRobux'] in config['Setting']['Prices']:
+                    Post()
+                else:
+                    warnings("Price Doesn't Match")
             else:
-                error('ERROR', 'RateLimit on economy Api.')
+                if info['PriceInRobux'] == 0:
+                    for _ in range(5):
+                        Post()
+        else:
+            error('ERROR', 'RateLimit on economy API.')
 
-        bar = quux
-
+    bar = quux
+                
